7b76b6883e833dfee2cd5b88a34c90a3bf043d62
usernotavailable123 <102803913+usernotavailable123@users.noreply.github.com>
2022-07-08 14:11:28 +0800

    add data from api using fetch

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -8,7 +8,7 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
             <div className="card" style={{width: "18rem"}}>
-              <img src={imageUrl} className="card-img-top" alt="..." />
+              <img src={imageUrl?imageUrl:'https://pbs.twimg.com/profile_images/1108430392267280389/ufmFwzIn_400x400.png'} className="card-img-top" alt="..." />
               <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
                 <p className="card-text">{description}...</p>
