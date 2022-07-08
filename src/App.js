683e3b56b13fccc1539387b99b48e7ec49228c74
usernotavailable123 <102803913+usernotavailable123@users.noreply.github.com>
2022-07-08 17:08:30 +0800

    added react router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { Routes, Route, Link } from "react-router-dom";
 
 import React, { Component } from 'react'
 import Navbar from './components/Navbar';
@@ -9,7 +10,17 @@ export default class App extends Component {
     return (
       <div>
         <Navbar />
-        <News pageSize={5} country = 'in' category = 'science'/>
+        <Routes>
+            {/* <News pageSize={5} country = 'in' category = 'science'/> */}
+            <Route path="/" element={<News key = 'general' pageSize={5} country = 'in' category = 'general'/>} />
+            <Route path="/business" element={<News key = 'business' pageSize={5} country = 'in' category ='business'/>}/>
+            <Route path="/entertainment" element={<News key = 'entertainment' pageSize={5} country = 'in' category ='entertainment'/>}/>
+            <Route path="/general" element={<News key = 'general' pageSize={5} country = 'in' category ='general'/>}/>
+            <Route path="/health" element={<News key = 'health' pageSize={5} country = 'in' category ='health'/>}/>
+            <Route path="/science" element={<News key = 'science' pageSize={5} country = 'in' category ='science'/>}/>
+            <Route path="/sports" element={<News key = 'sports' pageSize={5} country = 'in' category ='sports'/>}/>
+            <Route path="/technology" element={<News key = 'technology' pageSize={5} country = 'in' category ='technology'/>}/>  
+        </Routes>
       </div>
     )
   }
